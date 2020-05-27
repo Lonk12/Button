2910fa53431536756a38632f736b81c6cf7179d2
wolfyypaw12 <51008428+wolfyypaw12@users.noreply.github.com>
2020-05-27 15:19:28 -0400

    Add files via upload

---
+++ Window1.xaml.vb
@@ -0,0 +1,6 @@
+﻿Class Window1 
+
+    Private Sub Button1_Activated(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button1.Activated
+        My.Computer.Audio.Play("C:\Program Files (x86)\Button\Rick_Astley_-_Never_Gonna_Give_You_Up_Qoret.com.wav")
+    End Sub
+End Class
